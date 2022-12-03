554b8d541b3ef7378575ffa2853e6494e849ab7d
Rohan Reddy <rohanreddy888@gmail.com>
2022-12-03 12:45:03 +0530

    Minor Tweaks

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,7 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 export default function Navbar() {
   return (
-    <nav className="md:px-8 md:py-2 px-4 py-2 bg-white">
+    <nav className="md:px-8 md:py-2 px-4 py-2 bg-white shadow-md">
       <div className="flex flex-row justify-between items-center">
         <h1 className="text-2xl uppercase font-bold text-purple">Payzapp</h1>
         <ConnectButton />
