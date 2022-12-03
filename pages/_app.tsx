956ce4b7b732a64551bf87b560fe754187268084
chaitanyasjoshi <chaitanya9joshi@gmail.com>
2022-12-03 19:22:03 +0530

    fix: update config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,32 +3,17 @@ import "@rainbow-me/rainbowkit/styles.css";
 import type { AppProps } from "next/app";
 import { RainbowKitProvider, getDefaultWallets } from "@rainbow-me/rainbowkit";
 import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
-import { alchemyProvider } from "wagmi/providers/alchemy";
 import { publicProvider } from "wagmi/providers/public";
 import Layout from "../layout/Layout";
 
-const { chains, provider, webSocketProvider } = configureChains(
-  [
-    chain.mainnet,
-    chain.polygon,
-    chain.optimism,
-    chain.arbitrum,
-    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === "true"
-      ? [chain.goerli]
-      : []),
-  ],
-  [
-    // alchemyProvider({
-    //   // This is Alchemy's default API key.
-    //   // You can get your own at https://dashboard.alchemyapi.io
-    //   apiKey: '_gg7wSSi0KMBsdKnGVfHDueq6xMB9EkC',
-    // }),
-    publicProvider(),
-  ]
-);
+const {
+  chains: [, ...chains],
+  provider,
+  webSocketProvider,
+} = configureChains([chain.mainnet, chain.polygon, chain.arbitrum], [publicProvider()]);
 
 const { connectors } = getDefaultWallets({
-  appName: "RainbowKit App",
+  appName: "PayZapp",
   chains,
 });
 
