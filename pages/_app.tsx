6bd3100c15970f5b7472b3504fde69d12bdac812
chaitanyasjoshi <chaitanya9joshi@gmail.com>
2022-12-03 19:33:36 +0530

    feat: add avalance

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,40 @@
 import "../styles/globals.css";
 import "@rainbow-me/rainbowkit/styles.css";
 import type { AppProps } from "next/app";
-import { RainbowKitProvider, getDefaultWallets } from "@rainbow-me/rainbowkit";
+import { RainbowKitProvider, getDefaultWallets, Chain } from "@rainbow-me/rainbowkit";
 import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
-import { publicProvider } from "wagmi/providers/public";
+import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
 import Layout from "../layout/Layout";
+
+const avalancheChain: Chain = {
+  id: 43_114,
+  name: "Avalanche",
+  network: "avalanche",
+  iconUrl: "https://cryptologos.cc/logos/avalanche-avax-logo.svg?v=023",
+  iconBackground: "#fff",
+  nativeCurrency: {
+    decimals: 18,
+    name: "Avalanche",
+    symbol: "AVAX",
+  },
+  rpcUrls: {
+    default: "https://api.avax.network/ext/bc/C/rpc",
+  },
+  blockExplorers: {
+    default: { name: "SnowTrace", url: "https://snowtrace.io" },
+    etherscan: { name: "SnowTrace", url: "https://snowtrace.io" },
+  },
+  testnet: false,
+};
 
 const {
   chains: [, ...chains],
   provider,
   webSocketProvider,
-} = configureChains([chain.mainnet, chain.polygon, chain.arbitrum], [publicProvider()]);
+} = configureChains(
+  [chain.mainnet, chain.polygon, chain.arbitrum, avalancheChain],
+  [jsonRpcProvider({ rpc: (chain) => ({ http: chain.rpcUrls.default }) })]
+);
 
 const { connectors } = getDefaultWallets({
   appName: "PayZapp",
