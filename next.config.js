f28674e3a3df2a54490a3b39ace93f6a77a403ba
Rohan Reddy <rohanreddy888@gmail.com>
2022-12-04 02:53:52 +0530

    Added Pay

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["raw.githubusercontent.com", "static.debank.com"],
+    domains: ["raw.githubusercontent.com", "static.debank.com", "cryptologos.cc"],
   },
 };
 
